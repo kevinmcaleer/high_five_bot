d6a505702b10a8371efecef676aa8e83a2b291a2
Kevin McAleer <kevinmcaleer@gmail.com>
2024-12-22 14:25:37 +0000

    updated code

--- high_five.py
+++ high_five.py
@@ -9,35 +9,35 @@ from time import sleep
 range_finder = RangeFinder(trigger_pin=1, echo_pin=0)
 stepper = BYJ28([10,11,12,13])
 
-steps = 50
+direction = False
+hit_range = 20
+
+steps = 130
+delay = 0.001
 
 def high_five():
-    stepper.move_motor(steps, 1, 0.001)
-    sleep(0.1)
-    stepper.move_motor(steps, -1, 0.001)
+    stepper.move_motor(steps, direction, delay)
     sleep(0.1)
 
 def default_position():
-    stepper.move_motor(steps, 1, 0.001)
-    sleep(0.1)
-    stepper.move_motor(steps, -1, 0.001)
+    stepper.move_motor(steps, not direction, delay)
     sleep(0.1)
 
 was_in_high_five = False  # Flag to track the current position state
 
 while True:
     distance = range_finder.distance
-    print(f"Distance: {distance}, was high five: {was_in_high_five}")
+    print(f"Distance: {distance}")
 
-    if distance < 10:
+    if distance < hit_range:
         # If the arm is not already in high five position, do high five
         if not was_in_high_five:
             high_five()
-            was_in_high_five = True
+            was_in_high_five = True      
     else:
         # Only return to default position if the arm was previously in high five
         if was_in_high_five:
             default_position()
             was_in_high_five = False
-
-    sleep(0.1)
+    sleep(1)
+        
--- test_calibrate.py
+++ test_calibrate.py
@@ -1,25 +1,56 @@
 # test_calibrate
 
 from stepper_28byj48 import BYJ28
+from time import sleep
 
-motor  = BYJ28([10,11,12,13])
-direction = True
+stepper  = BYJ28([10,11,12,13])
+steps = 130
+direction = False
+delay = 0.001
 
-while True:
-    print("""
-        Press a for +10 steps
-        Press d for -10 steps
-        Press w to change direction
-        press q to quit
-""")
-    command = input(">")
-    if command in ['a','d','q']:
-        if command == 'a':
-            motor.move_motor(10,direction)
-        if command == 'w':
-            direction = not direction
-        if command == 'd':
-            motor.move_motor(10, (not direction))
-        if command == 'q':
-            break 
+def calibrate_position():
+    direction = True
+    
+    while True:
+        print("""
+            Press a for +10 steps
+            Press d for -10 steps
+            Press z for +1 step
+            Press c for - 1 step
+            Press w to change direction
+            press q to quit
+        """)
+        command = input(">")
+        if command in ['a','d','q','z','c']:
+            if command == 'a':
+                stepper.move_motor(1,direction)
+            if command == 'w':
+                direction = not direction
+            if command == 'z':
+                stepper.move_motor(1,direction)
+            if command == 'c':
+                stepper.move_motor(1,not direction)
+            if command == 'd':
+                stepper.move_motor(10, (not direction))
+            if command == 'q':
+                return 
+
+def calibrate_90():
+    direction = True 
+    stepper.move_motor(steps, not direction)
+    sleep(1)
+    stepper.move_motor(steps,  direction)
+
+def default_position():
+    stepper.move_motor(steps, not direction, delay)
+    sleep(0.1)
+
+# Un-comment the lines below to use the calibrate position, calibrate 90 degree
+# or to move the hand to the default position
+
+# calibrate_position()
+# calibrate_90()
+default_position()
+
+    
     
